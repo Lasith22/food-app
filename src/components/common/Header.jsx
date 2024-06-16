6ebf266a63c9dba1560e2cc2b4cd10d1d6bf69df
lasith herath <lasithherath00@gmail.com>
2024-06-16 12:43:16 +0530

    Fix marging issue

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div>
       <img src={FoodLogo} alt="" className="w-24 h-24" />
-      <div className="border-t border-gray-300 shadow-lg shadow-black mx-5 my-5"></div>
+      <div className="border-t border-gray-300 shadow-lg shadow-black mb-5"></div>
     </div>
   );
 };
