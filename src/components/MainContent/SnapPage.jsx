6b9465a68368e19e1bd70c3252f082c1b4404029
lasith herath <lasithherath00@gmail.com>
2024-06-17 12:42:27 +0530

    Testing

--- src/components/MainContent/SnapPage.jsx
+++ src/components/MainContent/SnapPage.jsx
@@ -1,4 +1,4 @@
-import { Image } from 'antd';
+import { Image, Tag } from 'antd';
 import React, { useRef, useState } from 'react';
 import Header from '../common/Header';
 import {
@@ -28,9 +28,6 @@ const SnapPage = () => {
       <Header />
       <div>
         <BrowserView>
-          <h1>This is rendered only in browser</h1>
-        </BrowserView>
-        <MobileView>
           <div className="flex flex-col items-center justify-center h-screen">
             <div
               className="relative w-72 h-72 border-2 border-dashed border-gray-300 rounded-lg flex items-center justify-center cursor-pointer overflow-hidden bg-cover bg-center"
@@ -57,6 +54,42 @@ const SnapPage = () => {
                   <p>Click a Snap and Track</p>
                 </div>
               )}
+            </div>
+          </div>
+        </BrowserView>
+        <MobileView>
+          <div className="flex flex-col items-center justify-start ">
+            <div className="flex flex-col items-start justify-start w-full max-w-md p-4">
+              <h1 className="mb-2 text-left text-[#181F26] text-[14px] font-sans font-bold">
+                Track in Less than 5 Seconds
+              </h1>
+
+              <div
+                className="relative w-full h-64 sm:h-72 md:h-96 lg:h-1/2 border-2 border-gray-300 rounded-lg flex items-center justify-center cursor-pointer overflow-hidden bg-cover bg-center"
+                style={{ backgroundImage: `url(${BackgroundImage})` }}
+                onClick={handleImageClick}
+              >
+                <input
+                  type="file"
+                  accept="image/*"
+                  capture="environment"
+                  onChange={handleFileChange}
+                  ref={fileInputRef}
+                  className="hidden"
+                />
+                {image ? (
+                  <img
+                    src={image}
+                    alt="Captured or Selected"
+                    className="w-full h-full object-cover rounded-lg"
+                  />
+                ) : (
+                  <div className="flex flex-col items-center justify-center text-white">
+                    <FaCamera size={50} className="mb-2" />
+                    <p>Click a Snap</p>
+                  </div>
+                )}
+              </div>
             </div>
           </div>
         </MobileView>
